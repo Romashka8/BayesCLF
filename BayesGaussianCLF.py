93ea96d1beac4edb827254fdca11526323eeab34
Roman <seleznyov.romario@mail.ru>
2024-08-24 15:55:55 +0300

    Adding Gaussian Naive Bayes CLF

---
+++ BayesGaussianCLF.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Union, Optional
+from mlxtend.plotting import plot_decision_regions
+
+
+class BayesGaussianCLF:
+    def __init__(self):
+        # classes priority's
+        self.priors = None
+        # arrays of classes mean values and std's
+        self.x_mean = None
+        self.x_std = None
+
+    def fit(self, x: Union[pd.DataFrame, np.array], y: Union[pd.Series, np.array]) -> None:
+        classes, cls_count = np.unique(y, return_counts=True)
+        n_classes = len(classes)
+        self.priors = cls_count / len(y)
+
+        # calculating mean and standard deviations of features by classes
+        self.x_mean = np.array([np.mean(x[y == cl], axis=0) for cl in range(n_classes)])
+        self.x_std = np.array([np.std(x[y == cl], axis=0) for cl in range(n_classes)])
+
+    # calculate the probability density of the feature according to the Gaussian distribution
+    @staticmethod
+    def pdf(x: float, mean: float, std: float) -> float:
+        return (1 / (np.sqrt(2 * np.pi) * std)) * np.exp(-0.5 * ((x - mean) / std) ** 2)
+
+    def predict(self, x: Union[pd.DataFrame, np.array]) -> np.array:
+        pdfs = np.array([BayesGaussianCLF().pdf(X, self.x_mean, self.x_std) for X in x])
+        posteriors = self.priors * np.prod(pdfs, axis=2)  # shorten Bayes formula
+
+        return np.argmax(posteriors, axis=1)
+
+
+# adding function for drawning graphics
+def decision_boundary_plot(x: Union[pd.DataFrame, np.array], y: Union[pd.Series, np.array],
+                           x_train: Union[pd.DataFrame, np.array], y_train: Union[pd.Series, np.array],
+                           clf: BayesGaussianCLF, feature_indexes: list[int], title: Optional[str]=None):
+    feature1_name, feature2_name = x.columns[feature_indexes]
+    X_feature_columns = x.values[:, feature_indexes]
+    X_train_feature_columns = x_train[:, feature_indexes]
+    clf.fit(X_train_feature_columns, y_train)
+
+    plot_decision_regions(X=X_feature_columns, y=y.values, clf=clf)
+    plt.xlabel(feature1_name)
+    plt.ylabel(feature2_name)
+    plt.title(title)
